d05170a847bd30f7f3979ad18319dbe2c0baf68b
Gary James <gjames@edgetechacademy.edu>
2019-11-13 20:34:58 -0600

    cripple the calculator

--- src/Calculator.java
+++ src/Calculator.java
@@ -16,28 +16,13 @@ public class Calculator {
 					num2 = Utils.getDouble("2nd # : ");
 					varCount = 2;
 					break;
-				case "cos":		case "rand":	case "sqrt":
-					num2 = Utils.getDouble("1st # : ");
-					varCount = 1;
-					break;
-				default: varCount = -1;
 			}
 			switch (operator) {
 				case "+"	:	answer = num1 + num2;	break;
 				case "-"	:	answer = num1 - num2;	break;
-				case "*"	:	answer = num1 * num2;	break;
-				case "/"	:	answer = num1 / num2;	break;
-				case "%"	:	answer = num1 % num2;	break;
-				case "pi"	:	answer = Math.PI;		break;
-				case "^"	:	answer = Math.pow(num1, num2);	break;
-				case "cos"	:	answer = Math.cos(num2);	break;
-				case "sqrt"	:	answer = Math.sqrt(num2);	break;
-				case "rand"	:	answer = Math.floor(Math.random()* num2);break;
 			}
 			switch (varCount) {
 				case 0	:	System.out.println(operator 			 + " = " + answer);					break;
-				case 1	:	System.out.println(operator + " " + num1 + " = " + answer);					break;
-				case 2	:	System.out.println(num1 + " " + operator + " " + num2 + " = " + answer);	break;
 			}
 		}
 	}
